8928b51057fca87254908b225b779c0e86e12a72
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-01-28 16:56:23 +0500

    Add files via upload

---
+++ LAB2.py
@@ -0,0 +1,5 @@
+# x2 = 5 we can not use a number as a first charcter in a variable name y = 'ábcd' no spaces bvetween varible names we cannot use - in the variable name
+x = 5
+y = 6
+z = x + y
+print(z)
