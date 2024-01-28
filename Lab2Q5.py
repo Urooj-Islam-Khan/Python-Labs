75d427d95e67f9d5f8febf63abd4bba28476a989
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-01-28 16:59:49 +0500

    Add files via upload

---
+++ Lab2Q5.py
@@ -0,0 +1,13 @@
+#  Write a program that initialize two variables with inateger (int) values and convert their data types to floating-point  (float), and displays them.
+
+age = 19
+items = 50
+
+print("age", age)
+print("items", items)
+
+age = float(age)
+items = float(items)
+
+print("age", age)
+print("items", items)
