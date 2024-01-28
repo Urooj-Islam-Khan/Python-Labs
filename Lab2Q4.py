6a695650f8970efbe95eaa8e3e7ca1a3c4ad8506
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-01-28 16:59:20 +0500

    Add files via upload

---
+++ Lab2Q4.py
@@ -0,0 +1,6 @@
+# Write a programs that input two numbers from the user, and  number raised to the power of the second number.
+
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+print(num1, "raised to power of ", num2, " is" , (num1**num2))
